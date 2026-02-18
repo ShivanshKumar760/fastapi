8e6bfe5596e4b0774790b5ff2290016fac8a4b61
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-02-19 04:10:51 +0530

    minor change

--- 05.middleware/StarletteMiddleware/main.py
+++ 05.middleware/StarletteMiddleware/main.py
@@ -5,19 +5,18 @@ from middleware.route_info import RouteInfoMiddleware
 app = FastAPI()
 app.add_middleware(LoggerMiddleware)
 app.add_middleware(RouteInfoMiddleware)
-
 @app.get("/")
 async def home():
-    return {"message": "Home route"}
+    return {"message": "Home Route"}
 
 @app.post("/create")
 async def create_item():
-    return {"message": "POST route"}
+    return {"message": "POST Route"}
 
 @app.put("/update")
 async def update_item():
-    return {"message": "PUT route"}
+    return {"message": "PUT Route"}
 
 @app.delete("/delete")
 async def delete_item():
-    return {"message": "DELETE route"}
+    return {"message": "DELETE Route"}
